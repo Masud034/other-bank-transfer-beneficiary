11bb05a4ad9f7b57fdb4d5d9690f43dea6e4c081
Masud034 <malam172034@bscse.uiu.ac.bd>
2022-08-24 14:55:22 +0600

    contraints added in entity, validators added in controller

--- src/main/java/com/example/otherbanktransferbeneficiary/controllers/AccountTransferController.java
+++ src/main/java/com/example/otherbanktransferbeneficiary/controllers/AccountTransferController.java
@@ -3,14 +3,18 @@ package com.example.otherbanktransferbeneficiary.controllers;
 import com.example.otherbanktransferbeneficiary.endpoints.EndpointUtils;
 import com.example.otherbanktransferbeneficiary.entities.AccountTransfer;
 import com.example.otherbanktransferbeneficiary.services.AccountTransferService;
+import com.example.otherbanktransferbeneficiary.validators.BeneficiaryIdMustExist;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class AccountTransferController {
@@ -20,22 +24,22 @@ public class AccountTransferController {
     @PostMapping(value = EndpointUtils.ADD_ACCOUNT_BENEFICIARY,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AccountTransfer> addBeneficiary(@PathVariable String userId, @RequestBody AccountTransfer accountTransfer) {
+    public ResponseEntity<AccountTransfer> addBeneficiary(@PathVariable String userId, @RequestBody @Valid AccountTransfer accountTransfer) {
         return new ResponseEntity<>(accountTransferService.addBeneficiary(userId, accountTransfer), HttpStatus.CREATED);
     }
 
     @GetMapping(value = EndpointUtils.GET_ACCOUNT_BENEFICIARY, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AccountTransfer> getBeneficiary(@PathVariable String userId, @PathVariable String beneficiaryId) {
+    public ResponseEntity<AccountTransfer> getBeneficiary(@PathVariable String userId, @PathVariable @BeneficiaryIdMustExist String beneficiaryId) {
         return new ResponseEntity<>(accountTransferService.getBeneficiary(userId, beneficiaryId), HttpStatus.OK);
     }
 
     @GetMapping(value = EndpointUtils.GET_ALL_ACCOUNT_BENEFICIARY, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<AccountTransfer>> getAllBeneficiaries(@PathVariable String userId) {
+    public ResponseEntity<List<AccountTransfer>> getAllBeneficiaries(@PathVariable @BeneficiaryIdMustExist String userId) {
         return new ResponseEntity<>(accountTransferService.getAllBeneficiaries(userId), HttpStatus.OK);
     }
 
     @DeleteMapping(value = EndpointUtils.DELETE_ACCOUNT_BENEFICIARY)
-    public ResponseEntity<String> deleteBeneficiary(@PathVariable String userId, @PathVariable String beneficiaryId) {
+    public ResponseEntity<String> deleteBeneficiary(@PathVariable String userId, @PathVariable @BeneficiaryIdMustExist String beneficiaryId) {
         accountTransferService.deleteBeneficiary(userId, beneficiaryId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- src/main/java/com/example/otherbanktransferbeneficiary/entities/AccountTransfer.java
+++ src/main/java/com/example/otherbanktransferbeneficiary/entities/AccountTransfer.java
@@ -7,12 +7,12 @@ import org.springframework.scheduling.support.SimpleTriggerContext;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructor
 @Entity
 public class AccountTransfer {
     @Id
@@ -22,17 +22,24 @@ public class AccountTransfer {
 
     private String userId;
 
+    @NotEmpty
     private String accountNumber;
 
+    @NotEmpty
     private String accountHolder;
 
+    @NotEmpty
     private String saveAsName;
 
+    @NotEmpty
     private String bankId;
 
+    @NotEmpty
     private String districtId;
 
+    @NotEmpty
     private String branchId;
 
+    @NotEmpty
     private String routingNumber;
 }
